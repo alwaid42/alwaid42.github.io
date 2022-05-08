8d10bf119fc643f821aed9294d4eb29d20aeb326
alwaid42 <ronaldomschmidt@gmail.com>
2022-05-08 00:25:50 -0300

    Preston Website added

---
+++ weather/js/script.js
@@ -0,0 +1,9 @@
+let date = new Date();
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursdays", "Friday", "Saturday"]
+let currentWeekday = weekdays[date.getDay()];
+let currentDay = date.getDate();
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+let currentMonth = months[date.getMonth()];
+let currentYear = date.getFullYear();
+let padronizedDate = currentWeekday+", "+currentDay+" "+currentMonth+" "+currentYear;
+document.querySelector('#date_time').textContent = padronizedDate;
