ecc906f4fb4448c8ef0ef8380874eda6fbca2332
alwaid42 <ronaldomschmidt@gmail.com>
2022-05-14 18:16:03 -0300

    commit

--- weather/js/script.js
+++ weather/js/script.js
@@ -1,15 +1,17 @@
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-
+/*
 function toggleMenu() {
     document
         .getElementsByClassName("navigation")[0]
         .classList.toggle("responsive");
 }
-
+*/
 
 let date = new Date();
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursdays", "Friday", "Saturday"]
